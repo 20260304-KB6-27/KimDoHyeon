8f026dbe5241cfd5bbe0c1ce9192aad278c338a3
dodo5517 <kh9015548@gmail.com>
2026-04-02 17:37:46 +0900

    10_Axios

---
+++ 03_VUE/01_basic/10_Axios/axios-test-app/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+// import App from './App.vue'
+import App2 from './App2.vue';
+import router from './router';
+
+const app = createApp(App2);
+
+app.use(router);
+
+app.mount('#app');
