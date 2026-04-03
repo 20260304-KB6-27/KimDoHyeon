9ca3c969d96bfa09f56fc8ec433b72da187dbb3a
dodo5517 <kh9015548@gmail.com>
2026-04-03 15:19:45 +0900

    11_Router_Axios

---
+++ 03_VUE/01_basic/11_Router_Axios/todolist/src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>존재하지 않는 경로</h2>
+  <p>요청경로 : {{ currentRoute.path }}</p>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const currentRoute = useRoute();
+</script>
+
+<style lang="scss" scoped></style>
