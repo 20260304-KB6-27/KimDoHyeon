4370a6d4483e77610a00f05feeee1a287a0451e9
dodo5517 <kh9015548@gmail.com>
2026-04-27 16:07:12 +0900

    멀티스레드

---
+++ 04_JAVA/ch08-thread/src/basic/ch14/lecture/threadPool/ThreadPoolExample.java
@@ -0,0 +1,27 @@
+package basic.ch14.lecture.threadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolExample {
+
+    // 스레드 풀 생성(스레드 5개)
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        for(int i = 0; i < 15; i++) {
+            final int taskNum = i;
+            // 스레드 풀에서 스레드 하나 꺼내서 일 시키기.
+            executorService.execute(() -> {
+                System.out.println("작업 " + taskNum + " 실행 중... (Thread: "
+                        + Thread.currentThread().getName() + ")");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+            });
+        }
+        
+        // 스레드풀 종료
+        executorService.shutdown();
+    }
+}
